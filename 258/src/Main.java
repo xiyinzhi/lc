8cf51294302e86e2b0fab8fc8f2f663dba659b67
xiyinzhi <452964062@qq.com>
2017-12-12 23:48:07 -0500

    258

---
+++ 258/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner input = new Scanner(System.in);
+        System.out.println("input:");
+        int num = input.nextInt();
+        System.out.println("output:" + m.addDigits(num));
+    }
+
+    public int addDigits(int num) {
+
+    }
+}
