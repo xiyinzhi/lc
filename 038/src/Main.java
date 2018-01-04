2208c3b49817b3ccc3444cba9717c41b4a5487b2
xiyinzhi <452964062@qq.com>
2018-01-04 18:25:38 +0800

    038

--- 038/src/Main.java
+++ 038/src/Main.java
@@ -12,9 +12,28 @@ public class Main {
 
     public String countAndSay(int n) {
         StringBuilder sb = new StringBuilder();
-        if(n == 1){
+        if (n == 1) {
             return sb.append("1").toString();
         }
-        char [] ch = countAndSay(n-1).toCharArray();
+        char[] ch = countAndSay(n - 1).toCharArray();
+        int num = ch.length;
+        char temp = ch[0];
+        int count = 0;
+        for (int i = 0; i < num; i++) {
+            if (ch[i] == temp) {
+                count += 1;
+            } else {
+                sb.append(String.valueOf(count));
+                sb.append(String.valueOf(temp));
+                count = 1;
+                temp = ch[i];
+            }
+            if (i == num - 1) {
+                sb.append(String.valueOf(count));
+                sb.append(String.valueOf(temp));
+            }
+        }
+        return sb.toString();
     }
+
 }
