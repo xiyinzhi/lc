274546e3cd1eb026c02043c78328d56ac8ca5b5c
xiyinzhi <452964062@qq.com>
2018-01-18 11:43:55 -0500

    482

--- 482/src/Main.java
+++ 482/src/Main.java
@@ -12,6 +12,22 @@ public class Main {
     }
 
     public String licenseKeyFormatting(String S, int K) {
-
+        String str = S.toUpperCase();
+        char[] ch = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int n = ch.length;
+        for (int i = 0; i < n; i++) {
+            if (ch[i] != '-') {
+                sb.append(ch[i]);
+            }
+        }
+        int n1 = sb.length();
+        int c = n1 % K;
+        for (int i = n1 / K - 1; i >= 0; i--) {
+            if (c != 0 || i != 0) {
+                sb.insert(c + i * K, '-');
+            }
+        }
+        return sb.toString();
     }
 }
