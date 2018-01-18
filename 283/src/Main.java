fb7fee2c9e60590bbe9795401fb64629a15c7e56
xiyinzhi <452964062@qq.com>
2018-01-18 00:47:55 -0500

    283

--- 283/src/Main.java
+++ 283/src/Main.java
@@ -11,12 +11,13 @@ public class Main {
         for (int i = 0; i < strings.length; i++) {
             nums[i] = Integer.parseInt(String.valueOf(strings[i]));
         }
-        m.moveZeroes(nums);
+        m.moveZeroes2(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + ", ");
         }
     }
 
+    // o(n^2)
     public void moveZeroes(int[] nums) {
         int count = 0;
         int n = nums.length;
@@ -31,6 +32,21 @@ public class Main {
         }
         for (int i = n - count; i < n; i++) {
             nums[i] = 0;
+        }
+    }
+
+    // o(n)
+    public void moveZeroes2(int[] nums) {
+        int n = nums.length;
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        for (int j = k; j < n; j++) {
+            nums[j] = 0;
         }
     }
 }
