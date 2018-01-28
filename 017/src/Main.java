fa50f019b3f5d4f51be583bc2ab7f1a203c5277c
xiyinzhi <452964062@qq.com>
2018-01-28 17:54:24 -0500

    017

---
+++ 017/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner input = new Scanner(System.in);
+        System.out.println("input:");
+        String digits = input.nextLine();
+        System.out.println("output:" + m.letterCombinations(digits));
+    }
+
+    private final static String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> letters = new ArrayList<>();
+        List<String> sbs = letterCombination(0, digits, letters);
+        return sbs;
+    }
+
+    public List<String> letterCombination(int offset, String digits, List<String> letters) {
+        int n = digits.length();
+        if (n == 0) {
+            return null;
+        }
+        if (offset == n) {
+            return letters;
+        }
+        char digitAtOffset = digits.charAt(offset);
+        String letter = KEYS[digitAtOffset - '0'];
+        char[] chars = letter.toCharArray();
+        List<String> newLetters = new ArrayList<>();
+        for (int i = 0; i < chars.length; i++) {
+            int j = letters.size();
+            if (letters.isEmpty()) {
+                newLetters.add(String.valueOf(chars[i]));
+            } else {
+                for (int k = 0; k < j; k++) {
+                    StringBuilder sb = new StringBuilder(letters.get(k));
+                    sb.append(chars[k]);
+                    newLetters.add(sb.toString());
+                }
+            }
+        }
+        return letterCombination(offset + 1, digits, newLetters);
+    }
+
+}
