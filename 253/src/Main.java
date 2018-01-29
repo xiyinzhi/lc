362d0786370f0bc08aa8f4f05ddd45125519a404
xiyinzhi <452964062@qq.com>
2018-01-29 14:03:09 -0500

    253

---
+++ 253/src/Main.java
@@ -0,0 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    //O(nlogn) mainly based on sorting
+    public int minMeetingRooms(Interval[] intervals) {
+        int count = 0;
+        int n = intervals.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        //O(nlogn)
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int endIterator = 0;
+        for (int i = 0; i < n; i++) {
+            if (starts[i] < ends[endIterator]) {
+                count++;
+            } else {
+                endIterator++;
+            }
+        }
+        return count;
+    }
+}
