bfde924cbe931182a25e9e3fa4ff1efd35aa24f3
xiyinzhi <452964062@qq.com>
2018-02-04 16:20:52 -0500

    203

--- 203/src/Main.java
+++ 203/src/Main.java
@@ -3,4 +3,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode node = head;
+        while (node != null) {
+            if (node.val == val){
+                deleteNode(node);
+            }
+            node = node.next;
+        }
+        return head;
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
 }
