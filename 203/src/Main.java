7395e291b1c79e0cc00dcc85df6945ec453775c2
xiyinzhi <452964062@qq.com>
2018-02-05 20:25:35 -0500

    203

--- 203/src/Main.java
+++ 203/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        ListNode head = new ListNode();
         System.out.println("Hello World!");
     }
 
@@ -16,7 +17,7 @@ public class Main {
     public ListNode removeElements(ListNode head, int val) {
         ListNode node = head;
         while (node != null) {
-            if (node.val == val){
+            if (node.val == val) {
                 deleteNode(node);
             }
             node = node.next;
@@ -25,8 +26,35 @@ public class Main {
     }
 
     public void deleteNode(ListNode node) {
-        node.val = node.next.val;
-        node.next = node.next.next;
+        if (node.next == null) {
+            //此处改变了node的值，但是返回main函数则显示无变化?why
+            node = null;
+        } else if (node.next.next == null) {
+            node.val = node.next.val;
+            node.next = null;
+        } else {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pointer = head;
+        while (pointer.next != null) {
+            if (pointer.next.val == val) {
+                pointer.next = pointer.next.next;
+            } else {
+                pointer = pointer.next;
+            }
+        }
+        if (head.val == val) {
+            return head.next;
+        } else {
+            return head;
+        }
     }
 
 }
