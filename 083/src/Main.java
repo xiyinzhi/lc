6687b40489bb1cdafd1c40153038b7f894a2983c
xiyinzhi <452964062@qq.com>
2018-02-06 01:40:38 -0500

    083

---
+++ 083/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * Time:O(n) Space:O(1)
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.next.val == p.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
