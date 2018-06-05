fd665f9ef7121451f76e0c3837774d191720a60b
xiyinzhi <452964062@qq.com>
2018-06-05 20:18:32 +0800

    148

---
+++ 148/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        ListNode head = m.defineLinkedList();
+        ListNode p = m.sortList(head);
+        while (p != null) {
+            System.out.println(p.val);
+            p = p.next;
+        }
+    }
+
+    public ListNode defineLinkedList() {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+        return head;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        return head;
+    }
+
+}
