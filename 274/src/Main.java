3510839c05d7292a8b3e70564172575606b65772
xiyinzhi <452964062@qq.com>
2018-06-13 19:03:05 +0800

    274

--- 274/src/Main.java
+++ 274/src/Main.java
@@ -32,6 +32,32 @@ public class Main {
         return h;
     }
 
-
+    // Time:O(n) Space:O(n)
+    public int hIndex2(int[] citations) {
+        if (citations == null || citations.length < 1) {
+            return 0;
+        }
+        //h-index wouldn't exceed the total number of articles
+        //for example, even it is cited[100, 98, 0, 1, 5], we will only care about the number of cites including or below 5
+        int n = citations.length; //n: number of articles
+        int[] count = new int[n + 1];
+        for (int i : citations) {
+            if (i < citations.length) {
+                count[i]++;
+            } else {
+                count[n]++;
+            }
+        }
+        int sum = 0;
+        int max = 0;
+        for (int i = n; i >= 0; i--) {
+            sum += count[i];
+            if (sum >= i) {
+                max = i;
+                break;
+            }
+        }
+        return max;
+    }
 
 }
