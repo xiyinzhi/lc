33a5da9c4549547151d0b4a5457d39760df8e896
xiyinzhi <452964062@qq.com>
2018-06-14 23:33:12 +0800

    164

--- 164/src/Main.java
+++ 164/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import static java.lang.Integer.MAX_VALUE;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,5 +23,51 @@ public class Main {
             }
         }
         return dif;
+    }
+
+    public int maximumGap2(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+
+        if (max == min || n == 2) {
+            return max - min;
+        }
+
+        int[] mins = new int[n];
+        int[] maxs = new int[n];
+        Arrays.fill(mins, MAX_VALUE);
+        Arrays.fill(maxs, -1);
+
+        for (int i = 0; i < n; i++) {
+            double temp = (double) (max - min) / (n - 1);
+            int index = (int) ((nums[i] - min) / temp);
+            mins[index] = Math.min(mins[index], nums[i]);
+            maxs[index] = Math.max(maxs[index], nums[i]);
+        }
+
+        int maxGap = (max - min) / (n - 1);
+        int tempMin = -1, tempMax = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxs[i] == -1) {
+                continue;
+            } else {
+                tempMin = mins[i];
+                if (tempMax != -1) {
+                    tempMin = mins[i];
+                    maxGap = Math.max(maxGap, tempMin - tempMax);
+                }
+                tempMax = maxs[i];
+            }
+        }
+        return maxGap;
     }
 }
