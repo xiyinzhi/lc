ece9495d3c08d4167e1a056afe305f05f1080ee5
xiyinzhi <452964062@qq.com>
2018-06-15 00:48:37 +0800

    767

--- 767/src/Main.java
+++ 767/src/Main.java
@@ -13,10 +13,31 @@ public class Main {
             counts[ch[i]]++;
         }
         for (int i = 97; i < 123; i++) {
-            if (counts[i] >= (n - 1) / 2){
+            if (counts[i] > (n + 1) / 2) {
                 return "";
             }
         }
-        return S;
+        char[] newCh = new char[n];
+        int j = 0, k = 1;
+        for (int i = 97; i < 123; i++) {
+            char temp = (char) i;
+            while (counts[i] != 0 && j < n) {
+                newCh[j] = temp;
+                j += 2;
+                counts[i]--;
+            }
+            while (counts[i] != 0 && k < n) {
+                newCh[k] = temp;
+                k += 2;
+                counts[i]--;
+            }
+        }
+
+        String str = new String();
+        for (int i = 0; i < n; i++) {
+            str += newCh[i];
+        }
+
+        return str;
     }
 }
