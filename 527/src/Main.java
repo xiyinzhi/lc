ee64bdb7004373a4c654b04799cca06a0d6c46c9
xiyinzhi <452964062@qq.com>
2018-06-15 18:20:46 +0800

    527

---
+++ 527/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public List<String> wordsAbbreviation(List<String> words) {
+        int N = words.size();
+        String[] ans = new String[N];
+        int[] prefix = new int[N];
+
+        for (int i = 0; i < N; ++i)
+            ans[i] = abbrev(words.get(i), 0);
+
+        for (int i = 0; i < N; ++i) {
+            while (true) {
+                Set<Integer> dupes = new HashSet();
+                for (int j = i + 1; j < N; ++j)
+                    if (ans[i].equals(ans[j]))
+                        dupes.add(j);
+
+                if (dupes.isEmpty()) break;
+                dupes.add(i);
+                for (int k : dupes)
+                    ans[k] = abbrev(words.get(k), ++prefix[k]);
+            }
+        }
+
+        return Arrays.asList(ans);
+    }
+
+    public String abbrev(String word, int i) {
+        int N = word.length();
+        if (N - i <= 3) return word;
+        return word.substring(0, i + 1) + (N - i - 2) + word.charAt(N - 1);
+    }
+
+}
