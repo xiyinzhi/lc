543ce98705871e879a8f8db1dc24cad2a87d67e4
xiyinzhi <452964062@qq.com>
2018-07-05 10:39:54 +0800

    844

---
+++ 844/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> stackS = new Stack<>();
+        Stack<Character> stackT = new Stack<>();
+        for (char c : S.toCharArray()) {
+            if (!stackS.isEmpty() && c == '#') {
+                stackS.pop();
+            } else if (c != '#') {
+                stackS.push(c);
+            }
+        }
+        for (char c : T.toCharArray()) {
+            if (!stackT.isEmpty() && c == '#') {
+                stackT.pop();
+            } else if (c != '#') {
+                stackT.push(c);
+            }
+        }
+        boolean flag = true;
+        while (!stackS.isEmpty() && !stackT.isEmpty()) {
+            if (stackS.pop() != stackT.pop()) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true && stackS.isEmpty() && stackT.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
