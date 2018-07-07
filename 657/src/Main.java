d3f5e768383faff409e58908f7fa26b53846330d
xiyinzhi <452964062@qq.com>
2018-07-07 15:21:14 +0800

    657

---
+++ 657/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Main m
+        System.out.println("Hello World!");
+    }
+
+    public boolean judgeCircle(String moves) {
+        int d1 = 0, d2 = 0;
+        for (char c : moves.toCharArray()) {
+            switch (c) {
+                case 'U':
+                    d1++;
+                    break;
+                case 'D':
+                    d1--;
+                    break;
+                case 'L':
+                    d2++;
+                    break;
+                case 'R':
+                    d2--;
+                    break;
+                default:
+                    System.out.println("wrong instruction!");
+                    break;
+            }
+        }
+        return d1 == 0 && d2 == 0;
+    }
+}
