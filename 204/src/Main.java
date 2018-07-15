58c62e55532d5b3ba4ef593c88b9a07b5fe97bcf
xiyinzhi <452964062@qq.com>
2018-07-15 23:08:50 +0800

    204

---
+++ 204/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    // Time:O(n^2) Space:O(n)
+    // Time Limit Exceeded
+    public int countPrimes(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 2; i < n; i++) {
+            boolean flag = true;
+            for (int key : set) {
+                if (i % key == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                set.add(i);
+            }
+        }
+        return set.size();
+    }
+
+    // amazing & best sol with Time:O(n) Space:O(n)
+    public class Solution {
+        public int countPrimes(int n) {
+            boolean[] notPrime = new boolean[n];
+            int count = 0;
+            for (int i = 2; i < n; i++) {
+                if (notPrime[i] == false) {
+                    count++;
+                    for (int j = 2; i * j < n; j++) {
+                        notPrime[i * j] = true;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
