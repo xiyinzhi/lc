7ad33c267770d99da2652ebc3ec6300a99677a94
xiyinzhi <452964062@qq.com>
2018-07-25 11:20:12 +0800

    100

--- 100/src/Main.java
+++ 100/src/Main.java
@@ -17,6 +17,22 @@ public class Main {
         }
     }
 
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p == null & q != null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
 
 
 }
