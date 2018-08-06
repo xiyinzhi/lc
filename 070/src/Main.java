d3568d914b59868e9ec9e78fe40110d6056ab17b
xiyinzhi <shixi_yinzhi@staff.weibo.com>
2018-08-06 23:51:39 +0800

    070

--- 070/src/Main.java
+++ 070/src/Main.java
@@ -3,4 +3,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public int climbStairs(int n) {
+        int[] odd = new int[n + 1];
+        int[] even = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            if (i == 1) {
+                odd[i] = 1;
+                even[i] = 0;
+            } else {
+                odd[i] = even[i - 1] + odd[i - 1];
+                even[i] = odd[i - 1];
+            }
+        }
+        return odd[n] + even[n];
+    }
 }
