207c4a1bfc97d4ede12b35c40ea0700a10589470
shixi_yinzhi <shixi_yinzhi@staff.weibo.com>
2018-08-14 00:54:22 +0800

    470

--- 082/src/Main.java
+++ 082/src/Main.java
@@ -3,4 +3,31 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode deleteDuplication(ListNode pHead)
+    {
+        ListNode p = pHead;
+        if (p == null) {
+            return p;
+        }
+        int val = p.val;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else {
+                val = p.val;
+                p = p.next;
+            }
+        }
+        return pHead;
+    }
 }
