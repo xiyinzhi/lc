5110f636fca26613d06466cbedd6463116f20fc5
xiyinzhi <452964062@qq.com>
2018-09-05 09:51:19 -0400

    151

---
+++ 151/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+    // Time:O(n) Space:O(n)
+    public String reverseWords(String s) {
+        String[] strings = s.split(" ");
+        int n = strings.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (!strings[n - i - 1].equals("")) {
+                sb.append(strings[n - i - 1]).append(" ");
+            }
+        }
+        // attention to len == 0
+        int len = sb.length();
+        if (len > 0) {
+            sb.deleteCharAt(len - 1);
+        }
+        return sb.toString();
+    }
+}
