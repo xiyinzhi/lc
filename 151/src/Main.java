c5514ac7737e03e5e32c1b484fd53609efee6db2
xiyinzhi <452964062@qq.com>
2018-09-06 14:55:21 -0400

    189

--- 151/src/Main.java
+++ 151/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hello World!");
     }
 
+    char[] str;
+    String s = str.toString();
 
     // Time:O(n) Space:O(n)
     public String reverseWords(String s) {
