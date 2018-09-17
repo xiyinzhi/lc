92640a72887ba296fe7081dc27ec8db9554feed3
xiyinzhi <452964062@qq.com>
2018-09-16 21:54:13 -0400

    328

---
+++ 328/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+
+    }
+}
