d7749eb92d4f4f4f09df0a3581c9250fdacb8ad3
xiyinzhi <452964062@qq.com>
2018-09-17 20:43:41 -0400

    328

--- 328/src/Main.java
+++ 328/src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.oddEvenList(main.newList());
         System.out.println("Hello World!");
+    }
+
+    public ListNode newList() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        return head;
     }
 
     public class ListNode {
@@ -13,7 +24,34 @@ public class Main {
         }
     }
 
+    // Time:O(n) Space:O(1)
     public ListNode oddEvenList(ListNode head) {
-
+        ListNode h1 = head;
+        ListNode h2 = null;
+        if (head != null) {
+            h2 = head.next;
+        }
+        if (h1 == null || h2 == null) {
+            return h1;
+        }
+        ListNode p = h2.next;
+        ListNode p1 = h1;
+        ListNode p2 = h2;
+        int i = 3;
+        while (p != null) {
+            if (i % 2 == 1) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            p = p.next;
+            i++;
+        }
+        p1.next = null;
+        p2.next = null;
+        p1.next = h2;
+        return h1;
     }
 }
