0ab4d76b034675d887c362c20fcb46af11d76abc
xiyinzhi <452964062@qq.com>
2018-11-03 23:56:47 -0400

    205

--- 205/src/Main.java
+++ 205/src/Main.java
@@ -28,4 +28,19 @@ public class Main {
         }
         return true;
     }
+
+    // Time:O(n) Space:O(n) best solution
+    public boolean isIsomorphic2(String s, String t) {
+        int[] pos1 = new int[256];
+        int[] pos2 = new int[256];
+        for (int i = s.length() - 1; i >= 0; --i) {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if (pos1[cs] != pos2[ct]) {
+                return false;
+            }
+            pos1[cs] = pos2[ct] = i;
+        }
+        return true;
+    }
 }
