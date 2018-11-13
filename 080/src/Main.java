5a3fd2a9cddd5a0194cb28c9c357e9c79eec4edc
xiyinzhi <452964062@qq.com>
2018-11-13 01:23:03 -0500

    080

---
+++ 080/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int p1 = 0;
+        int p2 = 0;
+        int n = nums.length;
+        int mark = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[p2]) {
+                mark = 0;
+            }
+            if (nums[i] != nums[p2] || mark < 2) {
+                nums[++p1] = nums[i];
+            }
+            mark++;
+            p2++;
+        }
+        return p1 + 1;
+    }
+}
