f3355729da3196b75abf40ad72150cfede228107
xiyinzhi <452964062@qq.com>
2019-03-18 15:55:55 -0400

    update

--- 125/src/Main.java
+++ 125/src/Main.java
@@ -30,4 +30,21 @@ public class Main {
         }
         return true;
     }
+
+    public boolean isPalindrome2(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toLowerCase().toCharArray()) {
+            if ((c <= 'z' && c >= 'a') || (c <= '9' && c >= '0')) {
+                sb.append(c);
+            }
+        }
+        String str = sb.toString();
+        int n = str.length();
+        for (int k = 0; k < n / 2; k++) {
+            if (str.charAt(k) != str.charAt(n - k - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
