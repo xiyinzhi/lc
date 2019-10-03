e07ce18360b65dc6b061e1df114d33362752b193
xiyinzhi <452964062@qq.com>
2019-10-03 16:46:43 -0400

    035

--- 035/src/Main.java
+++ 035/src/Main.java
@@ -1,38 +1,52 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
+    // 优化前, binary search
     // Time:O(logn) Space:O(1)
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if (n < 1) {
-            return 0;
-        }
         int low = 0;
         int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+            } else if (nums[mid] > target) {
                 high = mid - 1;
+                if (low > high) {
+                    // mid = low
+                    return mid;
+                }
+            } else {
+                low = mid + 1;
+                if (low > high) {
+                    return low;
+                }
             }
         }
-        if (low > 0 && target < nums[low - 1]) {
-            return high;
-        } else {
-            return low;
-        }
+        return -1;
     }
 
-
-
+    /**
+     * 其实返回全是low这个需要思考一下，简约！
+     */
+    public int searchInsert2(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
 }
