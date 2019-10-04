3431db1c056ae6c2cbf4005dce21c05994866ea4
xiyinzhi <452964062@qq.com>
2019-10-04 00:52:15 -0400

    069

--- 069/src/Main.java
+++ 069/src/Main.java
@@ -12,14 +12,33 @@ public class Main {
 
     public int binarySearch(int x, int low, int high) {
         if (low < high) {
-            int mid = (low + high) / 2+1;
+            int mid = (low + high) / 2 + 1;
             if (Math.pow(mid, 2) > x) {
-                return binarySearch(x, low, mid-1);
+                return binarySearch(x, low, mid - 1);
             } else {
                 return binarySearch(x, mid, high);
             }
         }
         return low;
+    }
+
+    // binary search
+    // time:O(logn), Space:O(1)
+    public int mySqrt2(int x) {
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long temp = (long) Math.pow(mid, 2);
+            if (temp == (long) x) {
+                return mid;
+            } else if (temp > (long) x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
     }
 
 }
