f41afff31c9304a9a931932eb2050b901fabfb84
xiyinzhi <452964062@qq.com>
2019-10-06 23:37:57 -0400

    977

---
+++ 977/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] A = {1};
+        System.out.println(sortedSquares(A));
+    }
+
+    public static int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] res = new int[n];
+        int p1 = 0;
+        int p2 = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (A[i] >= 0) {
+                p2 = i;
+                p1 = i - 1;
+                break;
+            }
+        }
+        int i = 0;
+        if (p2 == p1) {
+            p1 = p2 - 1;
+        }
+        while (p1 >= 0 && p2 <= n - 1) {
+            if (Math.abs(A[p2]) > Math.abs(A[p1])) {
+                res[i++] = A[p1] * A[p1];
+                p1--;
+            } else {
+                res[i++] = A[p2] * A[p2];
+                p2++;
+            }
+        }
+        while (p1 < 0 && p2 <= n - 1) {
+            res[i++] = A[p2] * A[p2];
+            p2++;
+        }
+        while (p2 > n - 1 && p1 >= 0) {
+            res[i++] = A[p1] * A[p1];
+            p1--;
+        }
+        return res;
+    }
+}
