54bb62804b73e55e671eb5242860050b360f9711
xiyinzhi <452964062@qq.com>
2019-10-09 17:18:14 -0400

    019

--- 019/src/Main.java
+++ 019/src/Main.java
@@ -13,6 +13,8 @@ public class Main {
         }
     }
 
+    // Time:O(n) Space:O(1)
+    // 2 pass
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode p1 = head;
         ListNode p2 = head;
@@ -29,5 +31,28 @@ public class Main {
         }
         p2.next = p2.next.next;
         return head;
+    }
+
+    // Time:O(n) Space:O(1)
+    // 1 pass
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode p1 = newHead;
+        ListNode p2 = newHead;
+        int count = 0;
+        while (p2 != null) {
+            if (count < n) {
+                p2 = p2.next;
+                count++;
+            } else {
+                if (p2.next == null) {
+                    p1.next = p1.next.next;
+                }
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+        }
+        return newHead.next;
     }
 }
