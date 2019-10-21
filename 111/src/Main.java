7e67af3ac6168b2903399a46054d162d9e419341
xiyinzhi <452964062@qq.com>
2019-10-21 00:33:55 -0400

    111

--- 111/src/Main.java
+++ 111/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -32,5 +35,32 @@ public class Main {
             }
 
         }
+    }
+
+    // BFS, Most Efficient solution!
+    // Time:O(n) Space:O(1) n:number of nodes
+    public int minDepth2(TreeNode root) {
+        if(root == null) return 0;
+        int depth = 1;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            // for each level
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null){
+                    return depth;
+                }
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 }
