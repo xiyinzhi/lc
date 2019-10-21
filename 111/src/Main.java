4e55f5836c3c7ddde0497415a399d2bda0070a20
xiyinzhi <452964062@qq.com>
2019-10-21 00:34:07 -0400

    111

--- 111/src/Main.java
+++ 111/src/Main.java
@@ -40,22 +40,22 @@ public class Main {
     // BFS, Most Efficient solution!
     // Time:O(n) Space:O(1) n:number of nodes
     public int minDepth2(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         int depth = 1;
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int size = q.size();
             // for each level
-            for(int i=0;i<size;i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                if(node.left == null && node.right == null){
+                if (node.left == null && node.right == null) {
                     return depth;
                 }
-                if(node.left != null){
+                if (node.left != null) {
                     q.offer(node.left);
                 }
-                if(node.right != null){
+                if (node.right != null) {
                     q.offer(node.right);
                 }
             }
