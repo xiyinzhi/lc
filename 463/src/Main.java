a7bf6ce7ac999621fc554f128f35121b9e5f0a13
xiyinzhi <452964062@qq.com>
2019-10-26 22:19:15 -0400

    463

--- 463/src/Main.java
+++ 463/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello World!");
     }
 
-    // Time:O(m*n) Space:O(m*n)
+    // Time:O(m*n) Space:O(1)
     public int islandPerimeter(int[][] grid) {
         int m = grid.length;
         if (m == 0) {
@@ -31,5 +31,32 @@ public class Main {
             }
         }
         return p;
+    }
+
+    // smarter al little bit
+    public int islandPerimeter2(int[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+        int n = grid.length;
+        if (n == 0) {
+            return 0;
+        }
+        int m = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    res += 4;
+                    if (i < n - 1 && grid[i + 1][j] == 1) {
+                        res -= 2;
+                    }
+                    if (j < m - 1 && grid[i][j + 1] == 1) {
+                        res -= 2;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
