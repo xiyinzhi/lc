a005e0f0bb32eaf8c11c7a1ba6e232d72592de1a
xiyinzhi <452964062@qq.com>
2019-11-27 23:52:35 -0500

    146

---
+++ 146/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    class LRUCache {
+        Map<Integer,Integer> map;
+
+        public LRUCache(int capacity) {
+            map = new LinkedHashMap<>();
+        }
+
+        public int get(int key) {
+
+        }
+
+        public void put(int key, int value) {
+
+        }
+    }
+}
