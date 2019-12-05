9f79656014f9b6eaa6295ddf8f065ee164851f0b
xiyinzhi <452964062@qq.com>
2019-12-04 19:47:53 -0500

    543

--- 543/src/Main.java
+++ 543/src/Main.java
@@ -18,7 +18,22 @@ public class Main {
      * recursively: max(maxDia(left),maxDia(right),maxDep(left)+maxDep(right))
      */
     public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter = 0;
+        maxDepth(root, 0);
+        return maxDiameter;
+    }
 
+    private int maxDiameter;
 
+    public int maxDepth(TreeNode root, int depth) {
+        if (root == null) {
+            return depth - 1;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        int maxLeft = maxDepth(left, depth + 1);
+        int maxRight = maxDepth(right, depth + 1);
+        maxDiameter = Math.max(maxDiameter, maxLeft + maxRight - 2 * depth);
+        return Math.max(maxLeft, maxRight);
     }
 }
