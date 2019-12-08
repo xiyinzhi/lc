90467e27039f978cec1e7bc2d1cba1003878578a
xiyinzhi <452964062@qq.com>
2019-12-07 22:12:20 -0500

    304

---
+++ 304/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    class NumMatrix {
+        private int preSum[][];
+
+        public NumMatrix(int[][] matrix) {
+            int n = matrix.length;
+            int m = matrix[0].length;
+            preSum = new int[n + 1][m + 1];
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1] - preSum[row1][col2 + 1];
+        }
+    }
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+}
